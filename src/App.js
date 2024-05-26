fb1730f1906fb269a6d1ece62f365da0dba3e9eb
twinklegoyal233 <twinklegoyal420@gmail.com>
2024-05-26 12:50:28 +0530

    Companies Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
+import Companies from "./components/Companies/Companies";
 
 function App() {
   return (
     <div className="App">
      <Header/>
      <Body/>
+     <Companies/>
     </div>
   );
 }
